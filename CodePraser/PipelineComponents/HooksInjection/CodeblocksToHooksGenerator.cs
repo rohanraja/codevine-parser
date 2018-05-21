185dc400b16d817e79116d56ada063c55e006fbc
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-05-21 07:46:19 +0530

    Refactor: abstracted out hook line templates to a separate class

--- CodePraser/PipelineComponents/HooksInjection/CodeblocksToHooksGenerator.cs
+++ CodePraser/PipelineComponents/HooksInjection/CodeblocksToHooksGenerator.cs
@@ -31,28 +31,16 @@ namespace CodePraser.PipelineComponents.HooksInjection
 			if (block == null)
 				return outP;
 			
-			outP.Add(MethodEnterHook(block.sourceFile.FilePath, block.methodName));
+			outP.Add(HookTemplates.MethodEnterHook(block.sourceFile.FilePath, block.methodName));
 
             foreach (var statement in block.Statements)
             {
-				var kvp = LineExecHook(statement.Location.StatementId, statement.LineNo);
+				var kvp = HookTemplates.LineExecHook(statement.Location.StatementId, statement.LineNo);
                 outP.Add(kvp);
 
             }
             return outP;
         }
 
-		// ToDo: Move these to a different static method class
-		private KeyValuePair<int, string> MethodEnterHook(string filePath, string methodName)
-        {
-			string expr = string.Format("var mrid = CodeHooks.Instance().OnMethodEnter(\"{0}\", \"{1}\");\n\n", filePath, methodName);
-            return new KeyValuePair<int, string>(0, expr);
-        }
-
-        private KeyValuePair<int, string> LineExecHook(int statementId, int lineNo)
-        {
-            string expr = string.Format("CodeHooks.Instance().LogLineRun(mrid, {0}, CodeHooks.Now());\n\n", lineNo);
-			return new KeyValuePair<int, string>(statementId, expr);
-        }
 	}
 }
