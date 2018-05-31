80908b378983eaa46bc95cabc79520e78323f516
Rohan Raja <roraja@microsoft.com>
2018-05-31 18:14:21 +0530

    ADded setter

---
+++ TestCSharpProject/WithSetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCSharpProject
+{
+    class WithSetter
+    {
+        public int testSetter
+        {
+            set
+            {
+                int b = 3;
+                b += 3;
+                b -= 4;
+            }
+        }
+    }
+}
