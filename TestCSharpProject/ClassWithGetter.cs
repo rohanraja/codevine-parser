1a093b115518779c68e16f9239c52f0292082dec
Rohan Raja <roraja@microsoft.com>
2018-05-31 17:46:48 +0530

    Added class with getter

---
+++ TestCSharpProject/ClassWithGetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCSharpProject
+{
+    class ClassWithGetter
+    {
+        public int testGetter
+        {
+            get
+            {
+                int a = 3;
+                return a;
+            }
+        }
+    }
+}
