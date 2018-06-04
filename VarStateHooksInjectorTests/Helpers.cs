a87cfef85e6e18658cf8c4ff0c9b64234d1a472e
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-06-05 01:22:24 +0530

    Added simple Roslyn unit test for new hook injection system

---
+++ VarStateHooksInjectorTests/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace VarStateHooksInjectorTests
+{
+    public class Helpers
+    {
+        public Helpers()
+        {
+        }
+
+		internal static MethodDeclarationSyntax ParseMethodSyntax(string testMethod)
+		{
+			var rt = SyntaxFactory.ParseSyntaxTree(testMethod).GetRoot();
+            MethodDeclarationSyntax methSyntax = rt.ChildThatContainsPosition(0).AsNode() as MethodDeclarationSyntax;
+			return methSyntax;
+		}
+	}
+}
---
+++ VarStateHooksInjectorTests/MethodRendererTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VarStateHooksInjectorTests
+{
+    [TestClass]
+    public class MethodRendererTests
+    {
+        [TestMethod]
+        public void TestRoslynWorks()
+        {
+			string testMethod = @""" 
+            void MethodA()
+            {
+                int count = 0;
+                count ++;
+            }
+            """;
+			MethodDeclarationSyntax methSyntax = Helpers.ParseMethodSyntax(testMethod);
+
+			Assert.IsTrue(methSyntax.Body.Statements.Count == 2);
+
+        }
+    }
+}
