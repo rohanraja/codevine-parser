a46bc1cee91a2dcfd57275d810ab05a225ef7fe4
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-06-05 04:03:10 +0530

    Added passing complex nested block test cases

--- VarStateHooksInjectorTests/MethodRendererTests.cs
+++ VarStateHooksInjectorTests/MethodRendererTests.cs
@@ -11,6 +11,91 @@ namespace VarStateHooksInjectorTests
     public class MethodRendererTests
     {
 		[TestMethod]
+        public void TestMethodWithMultiLevelIfBlock()
+        {
+            string testMethod = @""" 
+            void MethodA()
+            {
+                if(true)
+                {
+                    if(true)
+                    {
+                        int count = 0;
+                    }
+                    int midCount = 0;
+                }
+            }
+            """;
+
+            Dictionary<int, List<string>> renderingInfo = new Dictionary<int, List<string>>() { };
+			renderingInfo[0] = new List<string>(){
+				"IfHookCall()",
+				"0",
+			};
+			renderingInfo[1] = new List<string>(){
+				"IfHookCall()",
+                "0",
+				"AssignmentCall()",
+				"1",
+            };
+			renderingInfo[2] = new List<string>(){
+                "AssignmentCall()",
+                "0",
+            };
+
+            int expectedStatementCount = 8;
+
+            List<string> expectedStatementSubStrings = new List<string>(){
+				"IfHookCall()",
+				"if(true)",
+				"IfHookCall()",
+                "if(true)",
+				"AssignmentCall()",
+                "int midCount",
+				"AssignmentCall()",
+				"int count",
+            };
+
+            Helpers.RunBlockRenderTest(testMethod, renderingInfo, expectedStatementCount, expectedStatementSubStrings);
+        }
+
+		[TestMethod]
+		public void TestMethodWithSingleIfBlock()
+		{
+			string testMethod = @""" 
+            void MethodA()
+            {
+                if(true)
+                {
+                    int count = 0;
+                }
+            }
+            """;
+
+			Dictionary<int, List<string>> renderingInfo = new Dictionary<int, List<string>>() { };
+			renderingInfo[0] = new List<string>(){
+				"IfHookCall()",
+				"0",
+			};
+			renderingInfo[1] = new List<string>(){
+				"AssignmentCall()",
+				"0",
+			};
+
+			int expectedStatementCount = 4;
+
+			List<string> expectedStatementSubStrings = new List<string>(){
+				"IfHookCall()",
+				"if(true)",
+				"AssignmentCall()",
+				"int count",
+			};
+
+			Helpers.RunBlockRenderTest(testMethod, renderingInfo, expectedStatementCount, expectedStatementSubStrings);
+		}
+
+
+		[TestMethod]
         public void TestRenderingSimpleMethod_WithHookStatements()
         {
             string testMethod = @""" 
@@ -22,18 +107,18 @@ namespace VarStateHooksInjectorTests
             """;
 
             Dictionary<int, List<string>> renderingInfo = new Dictionary<int, List<string>>() { };
-			renderingInfo[0] = new List<string>(){
-				"0",
-				"int hook1 = 5;",
-				"1",
-				"int hook2 = 10"
-			};
+            renderingInfo[0] = new List<string>(){
+                "0",
+                "int hook1 = 5;",
+                "1",
+                "int hook2 = 10"
+            };
 
             int expectedStatementCount = 4;
 
             List<string> expectedStatementSubStrings = new List<string>(){
                 "int count",
-				"int hook1 = 5;",
+                "int hook1 = 5;",
                 "count ++",
                 "int hook2 = 10"
             };
