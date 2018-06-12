95ac96dae656eb7dda380c69289e82cd48c2512e
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-06-13 03:18:40 +0530

    UNTESTED support for getter and setter. Also some refactoring in coderunnerinfo collector

--- VarStateHooksInjector/CodeRunnerInfoCollector.cs
+++ VarStateHooksInjector/CodeRunnerInfoCollector.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using VarStateHooksInjector.Entities;
 using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
 
 namespace VarStateHooksInjector{
 	
@@ -26,36 +27,87 @@ namespace VarStateHooksInjector{
 
         internal CodeRunnerInfo Collect(AccessorDeclarationSyntax node)
         {
-            // Todo - implement this
-            return new CodeRunnerInfo();
+            // Todo - Add unit tests for this
+
+            var info = new CodeRunnerInfo();
+
+			try
+			{
+				info.Name = getAccessorName(node);
+			}
+			catch
+			{
+				
+			}
+
+			// Collect all the statements and block infos
+			info.blockInfo = collectBlocks(node.Body);
+
+			// Check if the method is static
+            info.IsStatic = CheckForStatic(node.Modifiers);
+
+			return info;
         }
+
 
 		private CodeRunnerInfo collectInfo(BaseMethodDeclarationSyntax node, string name, bool isConstructor = false )
 		{
 			
 			var info = new CodeRunnerInfo();
+
 			info.Name = name;
 			info.IsConstructor = isConstructor;
-            info.IsConstructor = false;
-            BlockInfoCollector blockInfo = new BlockInfoCollector(root);
-            info.blockInfo = blockInfo.Collect(node.Body);
 
-            // Check if the method is static
-            foreach (var mod in node.Modifiers)
-            {
-                if (mod.Text.ToLower().Contains("static"))
-                    info.IsStatic = true;
-            }
+            // Collect all the statements and block infos
+			info.blockInfo = collectBlocks(node.Body);
 
-            // Collect all method arguments
-            foreach (var param in node.ParameterList.Parameters)
-            {
-                var methArg = new MethodArgument { Name = param.Identifier.ToString() };
-                info.Arguments.Add(methArg);
-            }
+			// Check if the method is static
+			info.IsStatic = CheckForStatic(node.Modifiers);
 
-            // Todo - Add method arguments to info
+			// Collect all method arguments
+			info.Arguments = CollectArguments(node.ParameterList);
+
             return info;
 		}
+
+		private List<MethodArgument> CollectArguments(ParameterListSyntax parameterList)
+		{
+			List<MethodArgument> outp = new List<MethodArgument>(){} ;
+			foreach (var param in parameterList.Parameters)
+            {
+                var methArg = new MethodArgument { Name = param.Identifier.ToString() };
+                outp.Add(methArg);
+            }
+			return outp;
+		}
+
+		private bool CheckForStatic(SyntaxTokenList modifiers)
+		{
+			foreach (var mod in modifiers)
+            {
+				if (mod.Text.ToLower().Contains("static"))
+					return true;
+            }
+			return false;
+		}
+
+		private Dictionary<int, BlockInfo> collectBlocks(BlockSyntax body)
+		{
+			BlockInfoCollector blockInfo = new BlockInfoCollector(root);
+            return blockInfo.Collect(body);
+		}
+
+		private string getAccessorName(AccessorDeclarationSyntax par)
+        {
+            string methodName = "";
+            var attrPar = par.Parent.Parent as PropertyDeclarationSyntax;
+            string attrName = attrPar.Identifier.Text;
+            string mtype = "set";
+            if (par.Kind().ToString().ToLower().Contains("get"))
+                mtype = "get";
+            methodName = attrName + "." + mtype;
+            return methodName;
+        }
+
 	}
 }
